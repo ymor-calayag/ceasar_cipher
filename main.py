7c7f2a40eba84af75044087542fe43d38215054c
ymor-calayag <jeffreyymor.calayag@gmail.com>
2025-12-12 16:44:11 +0800

    Add logo display at program start

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from caesar_art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo)
 
 while True:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
